040053dae4d53788b759a21b2fa4cf887b286f79
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 18:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click", () => {
+  let para = document.getElementById("status");
+
+  para.innerHTML = "<h1>Entered Metaverse</h1>";
+});
